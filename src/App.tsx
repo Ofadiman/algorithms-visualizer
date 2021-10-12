db9a29e3bfec88f5346d9e0724c0675c878b2585
ofadiman <ofadiman@gmail.com>
2021-10-12 10:36:25 +0200

    feat: add sorting algorithms page
    - Add home page.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { CssBaseline } from '@mui/material'
 import React, { Fragment, ReactElement } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import { RoutePath } from './enums/RoutePath.enum'
 import { MainLayout } from './layouts/Main.layout'
+import { HomePage } from './pages/Home/Home.page'
 import { PathFindingAlgorithms } from './pages/PathFindingAlgorithms/PathFindingAlgorithms.page'
+import { SortingAlgorithmsPage } from './pages/SortingAlgorithms/SortingAlgorithms.page'
 import { TutorialOne } from './pages/TutorialOne.page'
 import { TutorialTwo } from './pages/TutorialTwo.page'
 
@@ -13,8 +16,14 @@ export const App = (): ReactElement => {
       <CssBaseline />
       <BrowserRouter>
         <Switch>
-          <Route exact={true} path={`/`}>
+          <Route exact={true} path={RoutePath.PathFindingAlgorithms}>
             <PathFindingAlgorithms />
+          </Route>
+          <Route exact={true} path={RoutePath.SortingAlgorithms}>
+            <SortingAlgorithmsPage />
+          </Route>
+          <Route exact={true} path={RoutePath.Home}>
+            <HomePage />
           </Route>
           <Route exact={true} path={`/tutorial-one`}>
             <MainLayout>
---
+++ src/pages/SortingAlgorithms/SortingAlgorithms.page.tsx
@@ -0,0 +1,25 @@
+import { Button } from '@mui/material'
+import React, { ReactElement } from 'react'
+import { Link } from 'react-router-dom'
+
+import { RoutePath } from '../../enums/RoutePath.enum'
+import { MainLayout } from '../../layouts/Main.layout'
+
+export const SortingAlgorithmsPage = (): ReactElement => {
+  return (
+    <MainLayout
+      toolbarContent={
+        <div>
+          <Button color={`inherit`} component={Link} to={RoutePath.PathFindingAlgorithms}>
+            {`Path finding algorithms`}
+          </Button>
+          <Button color={`inherit`} component={Link} to={RoutePath.SortingAlgorithms}>
+            {`Sorting algorithms`}
+          </Button>
+        </div>
+      }
+    >
+      <div>{`Sorting algorithms page`}</div>
+    </MainLayout>
+  )
+}
